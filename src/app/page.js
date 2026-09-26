2c066e064bc1a523c28715c09b798c8295475ba4
Saejin <saejinmx@gmail.com>
2026-09-26 15:24:22 +0200

    feat: add about section

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,13 @@ export default async function Page() {
     return (
         <main>
             <Hero />
-            <section className="about container mb-32">
-
+            <section className="about container my-32 flex gap-16">
+                <div className="flex-1">
+                    <h2 className="text-6xl pb-4">À propos</h2>
+                </div>
+                <div className="flex-1">
+                    <img src="/assets/diane_draw.png" alt className="w-auto h-[75vh] transform-[scaleX(-1)]" />
+                </div>
             </section>
             <section className="related-paintings">
                 <div className="container mb-12">
